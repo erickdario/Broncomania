e2c24429ece60556dbb8520c9c4e969d504a434c
Erick Dario Garcia Suarez <erick_dario2492@hotmail.com>
2015-02-26 22:16:27 -0600

    3 tabs, problems with main view

--- src/com/vertexmedia/broncomania/MainActivity.java
+++ src/com/vertexmedia/broncomania/MainActivity.java
@@ -28,6 +28,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
@@ -48,6 +49,10 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        //Remove title bar
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
         setContentView(R.layout.activity_main);
 
         // Create the adapter that will return a fragment for each of the three primary sections
